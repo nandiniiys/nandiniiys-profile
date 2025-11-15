4c75aee2e80f01e80dec76e29bfa3e4ee084c89f
Nandinii Yeleswarapu <nandiniiys@tamu.edu>
2025-11-14 20:20:07 -0600

    set up dynamic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -14,7 +14,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router basename="/nandiniiys-profile">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navbar />
         <main style={{ padding: '2rem' }}>
@@ -30,7 +30,7 @@ function App() {
         </main>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
